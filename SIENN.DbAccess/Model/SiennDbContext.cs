1e4ee49616d90f0bffe8a30f072f4e046c42f284
Yuriy Karpenko <molodoy@li.ru>
2017-12-28 17:54:26 +0200

    move an addition queries to context

--- SIENN.DbAccess/Model/SiennDbContext.cs
+++ SIENN.DbAccess/Model/SiennDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace SIENN.DbAccess.Model
 {
@@ -45,5 +47,65 @@ namespace SIENN.DbAccess.Model
 				.WithMany(i => i.ProductCategories)
 				.HasForeignKey(i => i.ProductId);
 		}
+
+		/*
+To created data scheme create queries which in turn will deliver following data:
+1. Unavailable products which delivery is expected in current month
+2. Available products that are assigned to more than one category
+3. Top 3 categories with info about numbers of available, assigned products with its mean price in category (top 3 should display categories which mean price is the highest)
+
+3. Лучшие 3 категории с информацией о количестве доступных, назначенных продуктов со средней ценой в категории (верхняя 3 должна отображать категории, которые означают, что цена является самой высокой)
+3. Топ 3 категории с информацией о количестве доступных, назначенных продукции с его средняя цена в категории (Топ 3 следует отображать категории, которые означают, Цена самая высокая)
+т.е.
+получить 3 категории с максимальной ценой продукта, включая:
+1 кол-во доступных продуктов
+2 продукты с их средней ценой в категории
+ */
+
+		public IQueryable<Product> Query1()
+		{
+			var d = DateTime.Today;
+			var d1 = new DateTime(d.Year, d.Month, 1);
+			var d2 = d1.AddMonths(1);
+
+			var q = Product
+				.Where(i => !i.IsAvailable)
+				.Where(i => i.DeliveryDate.HasValue && d1 < i.DeliveryDate && i.DeliveryDate > d2);
+
+			return q;
+		}
+
+		public IQueryable<Product> Query2()
+		{
+			var q = Product
+				.Include(i => i.ProductCategories)
+				.Where(i => i.ProductCategories.Count() > 1);
+
+			return q;
+		}
+
+		public IQueryable<object> Query3()
+		{
+			var q3_group = ProductCategories
+			.Include(i => i.Category)
+			.Include(i => i.Product)
+			.Select(i => new { c = i.Category.Name, p = i.Product })
+			.GroupBy(i => i.c)
+			;
+			var q = q3_group
+				.OrderByDescending(i => i.Max(p => p.p.Price))
+				.Take(3)
+				.Select(i => new
+				{
+					category = i.Key,
+					availableProducts = i.Count(p => p.p.IsAvailable),
+					products = i.GroupBy(x => x.p.Name).Select(x => new { name = x.Key, price = x.Average(p => p.p.Price) }),
+				})
+				//.ToArray()
+				;
+
+			return q;
+		}
+
 	}
 }
--- SIENN.DbAccess/Repositories/ProductRepository.cs
+++ SIENN.DbAccess/Repositories/ProductRepository.cs
@@ -46,54 +46,5 @@ namespace SIENN.DbAccess.Repositories
 			return query.Skip(start).Take(count);
 		}
 
-
-		/*
-To created data scheme create queries which in turn will deliver following data:
-1. Unavailable products which delivery is expected in current month
-2. Available products that are assigned to more than one category
-3. Top 3 categories with info about numbers of available, assigned products with its mean price in category (top 3 should display categories which mean price is the highest)
-
-3. Лучшие 3 категории с информацией о количестве доступных, назначенных продуктов со средней ценой в категории (верхняя 3 должна отображать категории, которые означают, что цена является самой высокой)
-3. Топ 3 категории с информацией о количестве доступных, назначенных продукции с его средняя цена в категории (Топ 3 следует отображать категории, которые означают, Цена самая высокая)
-т.е.
-получить 3 категории с максимальной ценой продукта, включая:
-	1 кол-во доступных продуктов
-	2 продукты с их средней ценой в категории
-		 */
-
-		private void TestQueries()
-		{
-			var d = DateTime.Today;
-			var d1 = new DateTime(d.Year, d.Month, 1);
-			var d2 = d1.AddMonths(1);
-
-			var q1 = _entities
-				.Where(i => !i.IsAvailable)
-				.Where(i => i.DeliveryDate.HasValue && d1 < i.DeliveryDate && i.DeliveryDate > d2);
-
-			var q2 = _entities
-				.Include(i => i.ProductCategories)
-				.Where(i => i.ProductCategories.Count > 1);
-
-			var q3_group = _context.Set<ProductCategories>()
-				.Include(i => i.Category)
-				.Include(i => i.Product)
-				.Select(i => new { c = i.Category.Name, p = i.Product })
-				.GroupBy(i => i.c)
-				//.ToArray()
-				;
-			var q3 = q3_group
-				.OrderByDescending(i => i.Max(p => p.p.Price))
-				.Select(i => new
-				{
-					category = i.Key,
-					availableProducts = i.Count(p => p.p.IsAvailable),
-					products = i.GroupBy(x => x.p.Name).Select(x => new { name = x.Key, price = x.Average(p => p.p.Price) }),
-				})
-				.Take(3)
-				.ToArray()
-				;
-
-		}
 	}
 }
